d2c0ab20be9dd7b3907a82dbb3677b9b1e72ad83
Unknown <J.Kraken24@hotmail.com>
2018-01-10 22:37:07 +0100

    add weather forecast 10day

--- myApp/src/pages/about/about.ts
+++ myApp/src/pages/about/about.ts
@@ -9,12 +9,9 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  weather = {
-    "icon" : "",
-    "nowTemp" : "",
-    "relative_humidity" : ""
-  };
   searchString = "";
+  weatherArray = [];
+  // weather = [];
   request: Observable<any>;
 
   constructor(public navCtrl: NavController, public httpClient: HttpClient) {}
@@ -23,17 +20,20 @@ export class AboutPage {
     if(document.querySelector("#searchButton").style.backgroundColor == ""){
       document.querySelector("#weather").style.visibility = "hidden";
       document.querySelector("#searchButton").style.backgroundColor = "#f4f4f4";
-      console.log(this.searchString);
-      this.request = this.httpClient.get('http://api.wunderground.com/api/137581351957bfb1/conditions/q/CA/'+this.searchString+'.json');
+      this.request = this.httpClient.get('http://api.wunderground.com/api/137581351957bfb1/forecast10day/q/CA/'+this.searchString+'.json');
+
       this.request
       .subscribe(data => {
+        let array = [];
+        data.forecast.simpleforecast.forecastday.forEach(function(element) {
+            console.log(element);
+            array.push(element);
+        });
+        this.weatherArray = array;
+        //this.weather = array;
         document.querySelector("#weather").style.visibility = "visible";
-        console.log('my data: ', data.current_observation);
-        this.weather.alreadySearch = true;
-        this.weather.icon = data.current_observation.icon_url;
-        this.weather.nowTemp = data.current_observation.temp_c;
         document.querySelector("#searchButton").style.backgroundColor = "";
-      })
+       })
     }
   }
 
